7f328fdabe5c46364cbb6f33046da48845fe15c0
abdullaqudrat <aqudrat630@gmail.com>
2019-05-27 12:11:43 -0600

    Adds delete schedule functionality

--- spec/requests/api/v1/schedule/schedule_request_spec.rb
+++ spec/requests/api/v1/schedule/schedule_request_spec.rb
@@ -40,4 +40,37 @@ describe 'Schedule API' do
     expect(json_response).to have_key(:name)
     expect(json_response[:name]).to eq("Abdulla's Planner")
   end
+  it 'can delete a schedule' do
+    name = "Abdulla's Planner"
+
+    post "/api/v1/schedule?name=#{name}"
+
+    expect(response).to be_successful
+
+    get "/api/v1/schedule"
+
+    expect(response).to be_successful
+
+    delete "/api/v1/schedule"
+
+    json_response = JSON.parse(response.body, symbolize_names: true)
+    expect(response).to be_successful
+    expect(json_response).to be_a(Hash)
+    expect(json_response).to have_key(:message)
+    expect(json_response[:message]).to eq("Deleted schedule")
+  end
+  it 'fails to delete a schedule that doesnt exist' do
+
+    delete "/api/v1/schedule"
+
+    delete "/api/v1/schedule"
+
+    json_response = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+
+    expect(json_response).to be_a(Hash)
+    expect(json_response).to have_key(:message)
+    expect(json_response[:message]).to eq("error")
+  end
 end
