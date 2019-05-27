b7378d823275730663c6592f717e5f7fcaca5b56
abdullaqudrat <aqudrat630@gmail.com>
2019-05-27 09:50:52 -0600

    Adds schedule create method in controller

--- app/controllers/api/v1/schedule_controller.rb
+++ app/controllers/api/v1/schedule_controller.rb
@@ -1,5 +1,16 @@
 class Api::V1::ScheduleController < ApplicationController
   def create
-    
+    new_schedule = Schedule.new(schedule_params)
+    if new_schedule.save
+      render json: new_schedule
+    else
+      render json: {message: "error"}
+    end
   end
+
+  private
+    def schedule_params
+      params.require(:schedule).permit(:name)
+    end
 end
+
