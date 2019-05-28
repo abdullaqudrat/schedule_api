acba2b7964bc16129e823aae996e6166841592ac
abdullaqudrat <aqudrat630@gmail.com>
2019-05-27 18:12:36 -0600

    Adds regex cleaning params
    Adds before action for cleaning params to appointment and schedule controller
    Adds spec to test both

--- spec/requests/api/v1/appointment/appointment_request_spec.rb
+++ spec/requests/api/v1/appointment/appointment_request_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
-describe 'Schedule API' do
+describe 'Appointment API' do
   let(:name) { "dentist" }
+  let(:complex_name) { "dentist-office-at-10" }
   let(:start_time) { 1 }
   let(:end_time) { 2 }
   let(:bad_start_time) { 2 }
@@ -20,6 +21,21 @@ describe 'Schedule API' do
   expect(json_response).to be_a(Hash)
   expect(json_response).to have_key(:message)
   expect(json_response[:message]).to eq("Added dentist to planner")
+  end
+  it 'can create a appointment with complex name' do
+
+  post "/api/v1/schedule?name=planner"
+
+  expect(response).to be_successful
+
+  post "/api/v1/schedule/planner/appointment?name=#{complex_name}&start_time=#{start_time}&end_time=#{end_time}"
+
+  json_response = JSON.parse(response.body, symbolize_names: true)
+
+  expect(response).to be_successful
+  expect(json_response).to be_a(Hash)
+  expect(json_response).to have_key(:message)
+  expect(json_response[:message]).to eq("Added dentist office at 10 to planner")
   end
   it 'fails to create a appointment with no params' do
 
@@ -80,6 +96,29 @@ describe 'Schedule API' do
     expect(json_response).to be_a(Hash)
     expect(json_response).to have_key(:name)
     expect(json_response[:name]).to eq("dentist")
+    expect(json_response).to have_key(:start_time)
+    expect(json_response[:start_time]).to eq("1")
+    expect(json_response).to have_key(:end_time)
+    expect(json_response[:end_time]).to eq("2")
+  end
+  it 'can show an appointment with complex name' do
+
+    post "/api/v1/schedule?name=planner"
+
+    expect(response).to be_successful
+
+    post "/api/v1/schedule/planner/appointment?name=#{complex_name}&start_time=#{start_time}&end_time=#{end_time}"
+
+    expect(response).to be_successful
+
+    get "/api/v1/schedule/planner/appointment/#{complex_name}"
+
+    json_response = JSON.parse(response.body, symbolize_names: true)
+  
+    expect(response).to be_successful
+    expect(json_response).to be_a(Hash)
+    expect(json_response).to have_key(:name)
+    expect(json_response[:name]).to eq("dentist office at 10")
     expect(json_response).to have_key(:start_time)
     expect(json_response[:start_time]).to eq("1")
     expect(json_response).to have_key(:end_time)
